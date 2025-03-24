cf486ba4d6f31236436f234884c1b2369ae9a86a
Jozsef Kiss <Jozsef.Kiss@redsailtechnologies.com>
2025-03-24 16:16:37 +0100

    feat(CONSTANTS): Add constant modules

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""Module containing constants used throughout the project."""
+
+COINGECKO_API_URL = "https://api.coingecko.com/api/v3/coins/markets"
+
+DB_CONFIG = {
+    "dbname": "crypto_db",
+    "user": "crypto_user",
+    "password": "crypto_pass",
+    "host": "localhost",
+    "port": 5432
+}
